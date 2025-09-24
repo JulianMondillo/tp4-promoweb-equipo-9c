0cf95c7e6a7f75217ae93b6433bec60a8641c881
Julián Mondillo <juli.mondi@hotmail.com>
2025-09-24 13:51:29 -0300

    Creacion y contenido de la clase AccesoDatos en la capa .Datos

---
+++ CatalogoProductos.Datos/AcessoDatos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoProductos.Datos
+{
+    internal class AcessoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AcessoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=PROMOS_DB; integrated security=true");
+            comando = new SqlCommand();
+            
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public void DefinirConsulta(string consultaSql)
+        {             
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consultaSql;
+        }
+
+        public void EjecutarConsulta()
+        {
+
+            try
+            {
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                if (conexion.State == ConnectionState.Closed) conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int EjecutarAccionEscalar()
+        {
+           comando.Connection = conexion;
+            try
+            {
+                if (conexion.State == ConnectionState.Closed) conexion.Open();
+                object id = comando.ExecuteScalar();
+                return (int)id;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        
+        }
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void LimpiarParametros()
+        {
+            comando.Parameters.Clear();
+        }
+
+
+    }
+}
